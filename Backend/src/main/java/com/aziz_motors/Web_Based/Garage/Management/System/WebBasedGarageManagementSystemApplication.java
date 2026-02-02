dfc98fad0e7e09fc7cb0917e8e7e794b22fa1dfa
Ansari Mohd Saad <ansarisaad777666@gmail.com>
2026-02-02 13:25:15 +0530

    Configured the DB and Mail, and ignored the application.properties for security and added application.properties.example .

--- Backend/src/main/java/com/aziz_motors/Web_Based/Garage/Management/System/WebBasedGarageManagementSystemApplication.java
+++ Backend/src/main/java/com/aziz_motors/Web_Based/Garage/Management/System/WebBasedGarageManagementSystemApplication.java
@@ -8,6 +8,7 @@ public class WebBasedGarageManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebBasedGarageManagementSystemApplication.class, args);
+		System.out.println("Spring Application Has Been Started...");
 	}
 
 }
