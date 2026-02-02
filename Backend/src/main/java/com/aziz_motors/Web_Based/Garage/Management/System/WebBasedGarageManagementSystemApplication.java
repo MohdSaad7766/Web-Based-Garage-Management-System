54c439a2bf5a98431a72e2e4bce32a270bd6c181
Ansari Mohd Saad <ansarisaad777666@gmail.com>
2026-02-02 12:17:45 +0530

    Initial Commit.

---
+++ Backend/src/main/java/com/aziz_motors/Web_Based/Garage/Management/System/WebBasedGarageManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aziz_motors.Web_Based.Garage.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedGarageManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBasedGarageManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/aziz_motors/Web_Based/Garage/Management/System/WebBasedGarageManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziz_motors.Web_Based.Garage.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedGarageManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
