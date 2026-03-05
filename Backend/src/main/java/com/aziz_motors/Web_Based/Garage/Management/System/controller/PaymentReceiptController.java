5162d49d304b24c9492f9e703e0701d9d7151459
Ansari Mohd Saad <ansarisaad777666@gmail.com>
2026-03-06 02:04:09 +0530

    Created PaymentReceipt Controller.

---
+++ Backend/src/main/java/com/aziz_motors/Web_Based/Garage/Management/System/controller/PaymentReceiptController.java
@@ -0,0 +1,33 @@
+package com.aziz_motors.Web_Based.Garage.Management.System.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment-receipt")
+public class PaymentReceiptController {
+
+    public void addPaymentReceipt(){
+
+    }
+
+    public void getPaymentReceiptById(){
+
+    }
+
+    public void getPaymentReceiptsByDealerId(){
+
+    }
+
+    public void getPaymentReceipts(){
+
+    }
+
+    public void updatePaymentReceipt(){
+
+    }
+
+    public void deletePaymentReceipt(){
+
+    }
+}
