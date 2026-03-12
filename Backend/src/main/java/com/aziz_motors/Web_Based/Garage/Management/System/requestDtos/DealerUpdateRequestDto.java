423cbd5c0f57e34861208cb58eb8db4153b47fd8
Ansari Mohd Saad <ansarisaad777666@gmail.com>
2026-03-13 04:10:22 +0530

    Updated PUT /payment-receipt api.

---
+++ Backend/src/main/java/com/aziz_motors/Web_Based/Garage/Management/System/requestDtos/DealerUpdateRequestDto.java
@@ -0,0 +1,44 @@
+package com.aziz_motors.Web_Based.Garage.Management.System.requestDtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class DealerUpdateRequestDto {
+    private UUID id;
+
+    private String name;
+
+    private String gstNumber;
+
+    private String panNumber;
+
+    private String address;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String country;
+
+    private String contactPersonName;
+    private String contactPersonPhone;
+    private String contactPersonEmail;
+
+
+    private String phoneNumber;
+    private String email;
+    private String website;
+
+    private String bankName;
+    private String bankAccountNumber;
+    private String bankIFSC;
+}
---
+++ Backend/src/main/java/com/aziz_motors/Web_Based/Garage/Management/System/requestDtos/EstimateUpdateRequestDto.java
@@ -0,0 +1,18 @@
+package com.aziz_motors.Web_Based.Garage.Management.System.requestDtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstimateUpdateRequestDto {
+
+    private UUID id;
+}
